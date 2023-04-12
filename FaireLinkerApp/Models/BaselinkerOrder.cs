cf4bc62760cded4a1b5ebcd26014f24a1b2b878c
mateusz <mateuszbronclik@gmail.com>
2023-04-12 16:15:50 +0200

    Refactor: Changed BaselinkerOrder Class

--- FaireLinkerApp/Models/BaselinkerOrder.cs
+++ FaireLinkerApp/Models/BaselinkerOrder.cs
@@ -16,7 +16,9 @@ namespace FaireLinkerApp.Models
         public string Address2 { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
+        public string State { get; set; }
         public string Country { get; set; }
+        public string CompanyName { get; set; }
         public List<BaselinkerOrderProduct> Products { get; set; }
         public string AdditionalField { get; set; }
     }
@@ -24,9 +26,14 @@ namespace FaireLinkerApp.Models
     internal class BaselinkerOrderProduct
     {
         public string ProductId { get; set; }
+        public string ProductOptionId { get; set; }
         public string Name { get; set; }
+        public string ProductOptionName { get; set; }
         public int Quantity { get; set; }
+        public string Sku { get; set; }
         public double Price { get; set; }
+        public bool IncludesTester { get; set; }
     }
 }
+
 
